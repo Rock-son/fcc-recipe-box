c2c268122c890511cdf9d5c4e594a29caebddcd2
Rok <roky.zz@gmail.com>
2017-03-24 13:17:41 +0100

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,2 @@
+import React from 'react';
+import {Button, ButtonGroup, ButtonToolbar, FormControl, FormGroup, Modal, ControlLabel} from 'react-bootstrap';
---
+++ src/components/modalBox.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import {Button, ButtonGroup, ButtonToolbar, FormControl, FormGroup, Modal, ControlLabel} from 'react-bootstrap';
+
+
